0918723ea3c08ed8e64632cbb1e9879b5718d6d6
TouseefAhmad <touseefktk22@gmail.com>
2020-08-17 11:35:53 +0500

    fourth Assignment

---
+++ module4_solution/script.js
@@ -0,0 +1,15 @@
+(function Speaker(window){
+ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+ for (var i =0 ; i<names.length ; i++) {
+
+
+  if (names[i].charAt(0) == "j" || names[i].charAt(0) == "J") {
+    console.log("Goodbye " + names[i]);
+  } else {
+    console.log("Hello " + names[i]);
+  }
+   
+}
+   window.names = names;
+})(window);
